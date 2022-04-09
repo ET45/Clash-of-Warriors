2ece7708ade5b546726d18e4d577593aec9b3ead
ET45 <emre.tugutlu@gmail.com>
2022-04-09 18:36:48 +0300

    Gravity done.

--- index.js
+++ index.js
@@ -22,11 +22,13 @@ class Sprite {
 
   update() {
     this.draw();
-    this.velocity.y += gravity;
+    /* this.velocity.y += gravity; */
     this.position.y += this.velocity.y;
 
     if (this.position.y + this.height + this.velocity.y >= canvas.height) {
       this.velocity.y = 0;
+    } else {
+      this.velocity.y += gravity;
     }
   }
 }
