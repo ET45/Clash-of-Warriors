c2c4505de792d06bbaaecc471be2667a356de5e7
ET45 <emre.tugutlu@gmail.com>
2022-04-09 17:51:41 +0300

    character and enemy graphiced

--- index.js
+++ index.js
@@ -5,3 +5,57 @@ canvas.width = 1024;
 canvas.height = 576;
 
 ctext.fillRect(0, 0, canvas.width, canvas.height);
+
+class Sprite {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+  }
+
+  draw() {
+    ctext.fillStyle = "red";
+    ctext.fillRect(this.position.x, this.position.y, 50, 150);
+  }
+
+  update() {
+    this.draw();
+    this.position.y += 10;
+  }
+}
+
+const player = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+});
+
+/* player.draw(); */
+
+const enemy = new Sprite({
+  position: {
+    x: 400,
+    y: 100,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+});
+
+/* enemy.draw(); */
+
+console.log(player);
+
+const animation = () => {
+  window.requestAnimationFrame(animation);
+  console.log("loop working?");
+  player.update();
+  enemy.update();
+};
+
+animation();
