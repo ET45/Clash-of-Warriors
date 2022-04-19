884aac18a5f0fd150e6faecd75289a6651cfc561
ET45 <emre.tugutlu@gmail.com>
2022-04-19 14:44:36 +0300

    flash problem solved but now stuck on first frame

--- index.js
+++ index.js
@@ -135,18 +135,21 @@ class Fighter extends Sprite {
         if (this.image !== this.sprites.idle.image) {
           this.image = this.sprites.idle.image;
           this.framesMax = this.sprites.idle.framesMax;
+          this.framesCurrent = 0;
         }
         break;
       case "run":
         if (this.image !== this.sprites.run.image) {
           this.image = this.sprites.run.image;
           this.framesMax = this.sprites.run.framesMax;
+          this.framesCurrent = 0;
         }
         break;
       case "jump":
         if (this.image !== this.sprites.jump.image) {
           this.image = this.sprites.jump.image;
           this.framesMax = this.sprites.jump.framesMax;
+          this.framesCurrent = 0;
         }
         break;
     }
